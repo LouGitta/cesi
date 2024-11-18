ad866d8a529572f4f979c35578f8060fa912ce36
Lou <lougitta@gmail.com>
2024-11-18 13:51:38 +0100

    Tp1 react

--- React/react_course/src/App.jsx
+++ React/react_course/src/App.jsx
@@ -1,4 +1,57 @@
+import { useState } from "react";
+
 export default function App() {
+  const [weight, setWeight] = useState('');
+  const [size, setSize] = useState('');
+  const [activeItem, setActiveItem] = useState({  
+    imc:'',
+    slice:'',
+    target:'',
+    bgcolor:'',
+  })
+
+  function handleImc(event){
+    event.preventDefault();
+  
+    let imc = Math.round(weight/(size*size))
+    let slice;
+    let target;
+    let bgcolor;
+
+    if ( imc < 18.5) {
+      slice = 'maigreur'
+      target = Math.round(18.5 * (size * size))
+      bgcolor = '#e46416'
+
+    } else if ( imc <= 25) {
+      slice = 'normal'
+      target = null
+      bgcolor = '#4a5093'
+
+    } else if ( imc < 30) {
+      slice = 'surpoids'
+      target = Math.round(25 * (size * size))
+      bgcolor = '#a82a7a'
+
+    } else if ( imc < 35) {
+      slice = 'obésité'
+      target = Math.round(25 * (size * size))
+      bgcolor = '#2d68c7'
+
+    } else if ( imc < 40) {
+      slice = 'obésité massive'
+      target = Math.round(25 * (size * size))
+      bgcolor = '#ba9ae5'
+
+    } else {
+      slice = 'obésité sévère'
+      target = Math.round(25 * (size * size))
+      bgcolor = '#18cb8d'
+    }
+
+    setActiveItem({imc:imc, target:target, slice:slice, bgcolor:bgcolor})
+  }
+
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -13,35 +66,44 @@ export default function App() {
         <div className="row">
           <div className="col-4 pt-4">
             <h1 className="h3">Calculer votre IMC</h1>
-            <form method="post">
+            <form onSubmit={handleImc}>
               <input
+                type="number"
                 aria-label="Poids"
                 name="poids"
                 className="form-control"
                 placeholder="Poids en kg."
+                value={weight}
+                onChange={(e) => setWeight(+e.target.value)}
               />
 
               <input
+                type="number"
+                step="0.01"
                 aria-label="Taille"
                 className="form-control mt-3"
                 name="taille"
-                placeholder="taille en m."
+                placeholder="Taille en m."
+                value={size}
+                onChange={(e) => setSize(+e.target.value)}
               />
 
               <button type="submit" className="btn btn-success my-3 col-12">
-                <i className="fa-solid fa-weight-scale"></i>
+                Valider
               </button>
             </form>
-            {
-              <div className="alert  mt-4" role="alert">
-                <h3>IMC : </h3>
-                <p></p>
+            { activeItem.imc && (
+              <div style={{backgroundColor:activeItem.bgcolor}} className="alert  mt-4" role="alert">
+                <h3>IMC :</h3>
+                <p>{activeItem.slice}</p>
+                {activeItem.target !== null && (
                 <span>
-                  <p>objectif </p>
-                  <p>nb kilo </p>
+                  <p>Vous devez être à {activeItem.target} kg</p>
+                  <p>Vous devez {activeItem.target === 'maigreur' ? `gagner ${activeItem.target-weight}` : activeItem.target !== 'normal' ? `perdre ${weight-activeItem.target}` : ''} kg</p>
                 </span>
+                )}
               </div>
-            }
+            )}
           </div>
         </div>
       </div>
